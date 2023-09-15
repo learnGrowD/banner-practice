17a8231d4f38fffa976a8098b28acc5c3cee7bc5
leanGrowD <dohagtae@dohagtaes-MacBook-Air-2.local>
2023-09-15 17:58:53 +0900

    commonWebViewController, commonWebView 추가

---
+++ Banner-practice/Feature/Web/CommonWebView.swift
@@ -0,0 +1,13 @@
+//
+//  CommonWebView.swift
+//  Banner-practice
+//
+//  Created by 도학태 on 2023/09/15.
+//
+
+import UIKit
+import WebKit
+
+class CommonWebView {
+
+}
---
+++ Banner-practice/Feature/Web/CommonWebViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommonWebViewController.swift
+//  Banner-practice
+//
+//  Created by 도학태 on 2023/09/15.
+//
+
+import UIKit
+import WebKit
+
+class CommonWebViewController {
+
+}
